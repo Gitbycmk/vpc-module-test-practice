1ae1ac8d9aed04cb426778d853577a189992428c
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2026-06-19 21:31:37 +0530

    Terraform VPC Test

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
 module "vpc" {
   source = "../terraform-aws-vpc-practice"
-  
+
   vpc_cidr = var.vpc_cidr
   project_name = var.project_name
   environment = var.environment
+  vpc_tags = var.vpc_tags
+
+  # Publuc Subnets
+  public_subnet_cidrs = var.public_subnet_cidrs
+
+  # Private Subnets
+  private_subnet_cidrs = var.private_subnet_cidrs
+
+  # Publuc Subnets
+  database_subnet_cidrs = var.database_subnet_cidrs
 }
